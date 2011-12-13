cca716ed4aa88c5b243916badaf1208229ea145c
Cameron Dutro <camertron@gmail.com>
2011-12-13 13:27:16 -0800

    Made small aesthetic change

--- lib/ext/calendars/date.rb
+++ lib/ext/calendars/date.rb
@@ -8,7 +8,7 @@ module TwitterCldr
   class LocalizedDate < LocalizedObject
     def method_missing(method, *args, &block)
       type = method.to_s.match(/to_(\w+)_s/)[1]
-      if type && type.size > 0
+      if type && !type.empty?
         @formatter.format(@base_obj, :type => type.to_sym)
       else
         raise "Method not supported"

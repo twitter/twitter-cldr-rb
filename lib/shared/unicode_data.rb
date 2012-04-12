ad05bf4735596302a8323ab7fc8f26f8c5fcaba8
Timothy Andrew <mail@timothyandrew.net>
2012-04-13 00:19:16 +0530

    Use a lazy getter to fetch the blocks, so it isn't automatically loaded into memory unless required.

--- lib/shared/unicode_data.rb
+++ lib/shared/unicode_data.rb
@@ -3,13 +3,12 @@
 module TwitterCldr
   module Shared
     class UnicodeData
-      @@blocks = TwitterCldr.resources.resource_for("unicode_data", "blocks")
       @@cache = {}
 
       class << self
         def for_code_point(code_point)
           #Find the target block
-          target = @@blocks.find do |block_name, range|
+          target = blocks.find do |block_name, range|
             range.cover? code_point.to_i(16)
           end 
 
@@ -18,6 +17,10 @@ module TwitterCldr
             @@cache[target_name.to_sym] ||= TwitterCldr.resources.resource_for("unicode_data", target_name)
             @@cache[target_name.to_sym][code_point.to_sym]
           end
+        end
+
+        def blocks
+          @blocks ||= TwitterCldr.resources.resource_for("unicode_data", "blocks")
         end   
       end
     end

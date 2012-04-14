a2f33f7e0f00d13c2bb34a2abc0261ed5c4076fa
Timothy Andrew <mail@timothyandrew.net>
2012-04-14 11:52:40 +0530

    Minor refactoring of #reorder

--- lib/normalizers/canonical/nfd.rb
+++ lib/normalizers/canonical/nfd.rb
@@ -5,8 +5,8 @@ module TwitterCldr
     class NFD < Base
       class << self
         def normalize_code_points(code_points)          
-          reorder(code_points)        
           code_points = code_points.map { |code_point| decompose code_point }.flatten
+          reorder code_points
           code_points
         end
 
@@ -26,22 +26,21 @@ module TwitterCldr
         end
 
         #Swap any two adjacent code points A & B if ccc(A) > ccc(B) > 0
-        def reorder(code_point_sequence)
-          combining_classes = code_point_sequence.map do |code_point| 
-            unicode_data = TwitterCldr::Shared::UnicodeData.for_code_point(code_point) || Array.new(size=15, obj="")
-            unicode_data[3].to_i
-          end
-
-          (code_point_sequence.size).times do
-            code_point_sequence.each_index do |i|
-              if code_point_sequence[i+1]
-                if (combining_classes[i] > combining_classes[i+1]) and (combining_classes[i+1] > 0)
-                  code_point_sequence[i], code_point_sequence[i+1] = code_point_sequence[i+1], code_point_sequence[i]
-                  combining_classes[i], combining_classes[i+1] = combining_classes[i+1], combining_classes[i]
+        def reorder(code_points)
+          (code_points.size).times do
+            code_points.each_with_index do |cp, i|
+              unless cp == code_points.last
+                ccc_a, ccc_b = combining_class_for(cp), combining_class_for(code_points[i+1])
+                if (ccc_a > ccc_b) and (ccc_b > 0)
+                  code_points[i], code_points[i+1] = code_points[i+1], code_points[i]
                 end
               end
             end
           end
+        end
+
+        def combining_class_for(code_point)
+          (TwitterCldr::Shared::UnicodeData.for_code_point(code_point) || Array.new(size=15, obj=""))[3].to_i
         end
       end
     end

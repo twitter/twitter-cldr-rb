d0de5b70751a9596842179cd86b9c0a63c590197
Timothy Andrew <tim@timothyandrew.net>
2012-04-29 02:07:14 +0530

    Rename `AttrNames` to `Attributes` https://github.com/twitter/twitter-cldr-rb/pull/19#issuecomment-5399343

--- lib/shared/unicode_data.rb
+++ lib/shared/unicode_data.rb
@@ -6,7 +6,7 @@
 module TwitterCldr
   module Shared
     class UnicodeData
-      AttrNames = Struct.new(:code_point, :name, :category, :combining_class, :bidi_class, :decomposition,
+      Attributes = Struct.new(:code_point, :name, :category, :combining_class, :bidi_class, :decomposition,
                              :digit_value, :non_decimal_digit_value, :numeric_value, :bidi_mirrored, :unicode1_name,
                              :iso_comment, :simple_uppercase_map, :simple_lowercase_map, :simple_titlecase_map)
       class << self
@@ -21,7 +21,7 @@ module TwitterCldr
           if target
             block_data = TwitterCldr.get_resource("unicode_data", target.first)          
             code_point_data = block_data.fetch(code_point.to_sym) { |code_point_sym| get_range_start(code_point_sym, block_data) }
-            AttrNames.new(*code_point_data) if code_point_data
+            Attributes.new(*code_point_data) if code_point_data
           end
         end
 

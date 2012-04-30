9d65586a2a54e46e67c899afc52752770112e999
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-05-01 00:43:48 +0300

    Use autoload.

--- lib/twitter_cldr.rb
+++ lib/twitter_cldr.rb
@@ -12,27 +12,17 @@ require 'date'
 require 'time'
 require 'forwardable'
 
-require 'version'
-
-# patches for extending Ruby functionality
-require 'twitter_cldr/core_ext/localized_object'
-require 'twitter_cldr/core_ext/calendars/datetime'
-require 'twitter_cldr/core_ext/calendars/date'
-require 'twitter_cldr/core_ext/calendars/time'
-require 'twitter_cldr/core_ext/numbers/localized_number'
-require 'twitter_cldr/core_ext/numbers/bignum'
-require 'twitter_cldr/core_ext/numbers/fixnum'
-require 'twitter_cldr/core_ext/numbers/float'
-require 'twitter_cldr/core_ext/strings/symbol'
-require 'twitter_cldr/core_ext/strings/string'
-
-require 'twitter_cldr/utils'
-
-# manages access to CLDR resources (yaml files in resources dir)
-require 'twitter_cldr/shared/resources'
+require 'twitter_cldr/version'
+require 'twitter_cldr/core_ext'
 
 
 module TwitterCldr
+
+  autoload :Formatters,  'twitter_cldr/formatters'
+  autoload :Normalizers, 'twitter_cldr/normalizers'
+  autoload :Shared,      'twitter_cldr/shared'
+  autoload :Tokenizers,  'twitter_cldr/tokenizers'
+  autoload :Utils,       'twitter_cldr/utils'
 
   extend SingleForwardable
 
@@ -94,40 +84,3 @@ module TwitterCldr
   end
 
 end
-
-
-# other shared libraries (most access shared resource data in resources/shared)
-require 'twitter_cldr/shared/currencies'
-require 'twitter_cldr/shared/languages'
-require 'twitter_cldr/shared/unicode_data'
-
-# all tokenizers
-require 'twitter_cldr/tokenizers/base'
-require 'twitter_cldr/tokenizers/composite_token'
-require 'twitter_cldr/tokenizers/key_path'
-require 'twitter_cldr/tokenizers/token'
-require 'twitter_cldr/tokenizers/calendars/datetime_tokenizer'
-require 'twitter_cldr/tokenizers/calendars/date_tokenizer'
-require 'twitter_cldr/tokenizers/calendars/time_tokenizer'
-require 'twitter_cldr/tokenizers/numbers/number_tokenizer'
-
-# all formatters
-require 'twitter_cldr/formatters/base'
-require 'twitter_cldr/formatters/calendars/datetime_formatter'
-require 'twitter_cldr/formatters/calendars/date_formatter'
-require 'twitter_cldr/formatters/calendars/time_formatter'
-require 'twitter_cldr/formatters/numbers/number_formatter'
-require 'twitter_cldr/formatters/numbers/decimal_formatter'
-require 'twitter_cldr/formatters/numbers/currency_formatter'
-require 'twitter_cldr/formatters/numbers/percent_formatter'
-require 'twitter_cldr/formatters/plurals/plural_formatter'
-require 'twitter_cldr/formatters/plurals/rules'
-
-# formatter helpers
-require 'twitter_cldr/formatters/numbers/helpers/base'
-require 'twitter_cldr/formatters/numbers/helpers/fraction'
-require 'twitter_cldr/formatters/numbers/helpers/integer'
-
-# all normalizers
-require 'twitter_cldr/normalizers/base'
-require 'twitter_cldr/normalizers/canonical/nfd'
---
+++ lib/twitter_cldr/shared.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+# Copyright 2012 Twitter, Inc
+# http://www.apache.org/licenses/LICENSE-2.0
+
+module TwitterCldr
+  module Shared
+    autoload :Currencies,  'twitter_cldr/shared/currencies'
+    autoload :Languages,   'twitter_cldr/shared/languages'
+    autoload :Resources,   'twitter_cldr/shared/resources'
+    autoload :UnicodeData, 'twitter_cldr/shared/unicode_data'
+  end
+end

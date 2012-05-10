856845bbaa2bf54851776e9a7e0785a29efd8b73
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-05-10 21:49:24 +0300

    Some minor fixes and changes for Normalizers::NFD.

--- lib/twitter_cldr/normalizers/canonical/nfd.rb
+++ lib/twitter_cldr/normalizers/canonical/nfd.rb
@@ -6,48 +6,48 @@
 module TwitterCldr
   module Normalizers
     class NFD < Base
-      @@hangul_constants = {:SBase => "AC00".hex, :LBase => "1100".hex, :VBase => "1161".hex, :TBase => "11A7".hex,
-                            :Scount => 11172, :LCount => 19, :VCount => 21, :TCount => 28, :NCount => 588, :Scount => 1172}
+
+      HANGUL_CONSTANTS = {
+          :SBase  => 0xAC00,
+          :LBase  => 0x1100,
+          :VBase  => 0x1161,
+          :TBase  => 0x11A7,
+          :LCount => 19,
+          :VCount => 21,
+          :TCount => 28,
+          :NCount => 588,  # VCount * TCount
+          :Scount => 11172 # LCount * NCount
+      }
+
       class << self
+
         def normalize(string)
-          #Convert string to code points
+          # Convert string to code points
           code_points = string.split('').map { |char| char_to_code_point(char) }
 
-          #Normalize code points
+          # Normalize code points
           normalized_code_points = normalize_code_points(code_points)
 
-          #Convert normalized code points back to string
+          # Convert normalized code points back to string
           normalized_code_points.map { |code_point| code_point_to_char(code_point) }.join
         end
 
         def normalize_code_points(code_points)
           code_points = code_points.map { |code_point| decompose code_point }.flatten
-          reorder code_points
+          reorder(code_points)
           code_points
         end
 
-        #Recursively replace the given code point with the values in its Decomposition_Mapping property
+        # Recursively replace the given code point with the values in its Decomposition_Mapping property.
         def decompose(code_point)
           unicode_data = TwitterCldr::Shared::UnicodeData.for_code_point(code_point)
           return code_point unless unicode_data
+
           decomposition_mapping = unicode_data.decomposition.split
 
-          # Special decomposition for Hangul syllables.
-          # Documented in Section 3.12 at http://www.unicode.org/versions/Unicode6.1.0/ch03.pdf
-          if unicode_data.name.include? 'Hangul'
-            sIndex = code_point.hex - @@hangul_constants[:SBase]
-
-            lIndex = sIndex / @@hangul_constants[:NCount]
-            vIndex = (sIndex % @@hangul_constants[:NCount]) / @@hangul_constants[:TCount]
-            tIndex = sIndex % @@hangul_constants[:TCount]
-
-            lPart = (@@hangul_constants[:LBase] + lIndex).to_s(16).upcase
-            vPart = (@@hangul_constants[:VBase] + vIndex).to_s(16).upcase
-            tPart = (@@hangul_constants[:TBase] + tIndex).to_s(16).upcase if tIndex > 0
-
-            [lPart, vPart, tPart].compact
-
-          #Return the code point if compatibility mapping or if no mapping exists
+          if unicode_data.name.include?('Hangul')
+            decompose_hangul(code_point)
+          # Return the code point if compatibility mapping or if no mapping exists
           elsif decomposition_mapping.first =~ /<.*>/ || decomposition_mapping.empty?
             code_point
           else
@@ -57,14 +57,34 @@ module TwitterCldr
           end
         end
 
-        #Swap any two adjacent code points A & B if ccc(A) > ccc(B) > 0
+        private
+
+        # Special decomposition for Hangul syllables.
+        # Documented in Section 3.12 at http://www.unicode.org/versions/Unicode6.1.0/ch03.pdf
+        def decompose_hangul(code_point)
+          s_index = code_point.hex - HANGUL_CONSTANTS[:SBase]
+
+          l_index = s_index / HANGUL_CONSTANTS[:NCount]
+          v_index = (s_index % HANGUL_CONSTANTS[:NCount]) / HANGUL_CONSTANTS[:TCount]
+          t_index = s_index % HANGUL_CONSTANTS[:TCount]
+
+          result = []
+
+          result << (HANGUL_CONSTANTS[:LBase] + l_index).to_s(16).upcase
+          result << (HANGUL_CONSTANTS[:VBase] + v_index).to_s(16).upcase
+          result << (HANGUL_CONSTANTS[:TBase] + t_index).to_s(16).upcase if t_index > 0
+
+          result
+        end
+
+        # Swap any two adjacent code points A & B if ccc(A) > ccc(B) > 0.
         def reorder(code_points)
-          (code_points.size).times do
+          code_points.size.times do
             code_points.each_with_index do |cp, i|
               unless i == (code_points.size - 1)
-                ccc_a, ccc_b = combining_class_for(cp), combining_class_for(code_points[i+1])
+                ccc_a, ccc_b = combining_class_for(cp), combining_class_for(code_points[i + 1])
                 if (ccc_a > ccc_b) && (ccc_b > 0)
-                  code_points[i], code_points[i+1] = code_points[i+1], code_points[i]
+                  code_points[i], code_points[i + 1] = code_points[i + 1], code_points[i]
                 end
               end
             end
@@ -72,13 +92,13 @@ module TwitterCldr
         end
 
         def combining_class_for(code_point)
-          begin
-            unicode_data = TwitterCldr::Shared::UnicodeData.for_code_point(code_point).combining_class.to_i
-          rescue NoMethodError
-            0
-          end
+          TwitterCldr::Shared::UnicodeData.for_code_point(code_point).combining_class.to_i
+        rescue NoMethodError
+          0
         end
+
       end
+
     end
   end
 end

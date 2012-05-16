c66e7109aad33c26e935b380027323e7bd439c23
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-05-17 00:09:28 +0300

    Polish full normalization tests.

--- spec/normalizers/normalization_spec.rb
+++ spec/normalizers/normalization_spec.rb
@@ -19,72 +19,77 @@ describe 'Unicode Normalization Algorithms' do
 
   shared_examples_for 'a normalization algorithm' do
     it 'passes all the tests in NormalizersTestShort.txt' do
-      run_short_test(invariants)
+      run_normalization_test(described_class, invariants, SHORT_TEST_PATH)
     end
 
     it 'passes all the tests in NormalizersTest.txt', :slow => true do
-      run_full_test(invariants)
+      run_normalization_test(described_class, invariants, FULL_TEST_PATH)
     end
   end
 
-  describe 'NFD' do
-    let(:invariants) {
-      lambda do |data|
-        NFD.normalize_code_points(data[0]).should == data[2]
-        NFD.normalize_code_points(data[1]).should == data[2]
-        NFD.normalize_code_points(data[2]).should == data[2]
-
-        NFD.normalize_code_points(data[3]).should == data[4]
-        NFD.normalize_code_points(data[4]).should == data[4]
-      end
-    }
-
+  describe NFD do
+    let(:invariants) { { 3 => [1, 2, 3], 5 => [4, 5] } }
     it_behaves_like 'a normalization algorithm'
   end
 
-  describe 'NFKD' do
-    let(:invariants) {
-      lambda do |data|
-        NFKD.normalize_code_points(data[0]).should == data[4]
-        NFKD.normalize_code_points(data[1]).should == data[4]
-        NFKD.normalize_code_points(data[2]).should == data[4]
-        NFKD.normalize_code_points(data[3]).should == data[4]
-        NFKD.normalize_code_points(data[4]).should == data[4]
-      end
-    }
-
+  describe NFKD do
+    let(:invariants) { { 5 => [1, 2, 3, 4, 5] } }
     it_behaves_like 'a normalization algorithm'
   end
 
-  def run_short_test(invariants)
-    open(SHORT_TEST_PATH, 'r:UTF-8') do |file|
-      run_normalization_test(file, invariants)
+  # Runs standard Unicode normalization tests from `file_path` for a given `normalizer`. Expected invariants are
+  # specified via `invariants` hash.
+  #
+  # E.g., if `invariants` is { 2 => [1, 2, 3], 4 => [4, 5] } than the following invariants are expected to be true:
+  #
+  #   c2 == normalized(c1) == normalized(c2) == normalized(c3)
+  #   c4 == normalized(c4) == normalized(c5)
+  #
+  # where (c1, c2,...) are columns of the normalization test separated by semicolons and normalized() is the
+  # normalization function. Note, how expectation and tests columns indexes match the numbers in the `invariants` hash.
+  #
+  def run_normalization_test(normalizer, invariants, file_path)
+    open(file_path, 'r:UTF-8') do |file|
+      file.each do |line|
+        next if line.empty? || line =~ /^(@|#)/
+
+        data = line.split(';')[0...5].map { |cps| cps.split }
+
+        invariants.each do |expected_index, tests|
+          expected = data[expected_index - 1]
+
+          tests.each do |test_index|
+            test = data[test_index - 1]
+
+            normalized = normalizer.normalize_code_points(test)
+
+            message = normalization_error_message(line, test, expected, normalized, test_index, expected_index)
+            normalized.should(eq(expected), message)
+          end
+        end
+      end
     end
   end
 
-  def run_full_test(invariants)
-    prepare_full_test
-
-    open(FULL_TEST_PATH, 'r:UTF-8') do |file|
-      run_normalization_test(file, invariants)
-    end
+  # Generates helpful error message for normalization test failure.
+  #
+  def normalization_error_message(line, test, expected, normalized, test_index, expected_index)
+    <<-END
+Test:       "#{line.strip}"
+Invariant:  normalized(c#{test_index}) == c#{expected_index}
+Expected:   normalized(#{test.inspect}) == #{expected.inspect}
+Got:        #{normalized.inspect}
+    END
   end
 
+  # Downloads full Unicode normalization tests suit if necessary.
+  #
   def prepare_full_test
     return if File.file?(FULL_TEST_PATH)
 
     print '    Downloading NormalizationTest.txt ... '
     open(FULL_TEST_PATH, 'w') { |file| file.write(open(NORMALIZATION_TEST_URL).read) }
     puts 'done.'
-  end
-
-  def run_normalization_test(file, invariants)
-    file.each do |line|
-      next if line.empty? || line =~ /^(@|#)/
-
-      data = line.split(';')[0...5].map { |cps| cps.split }
-      invariants.call(data)
-    end
   end
 
 end

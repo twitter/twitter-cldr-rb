dd2c2fbb56848b77910ab7c0bf9af3aeb7fbf570
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-06-01 10:20:01 +0300

    Update Resources#get_resource spec.

--- spec/shared/resources_spec.rb
+++ spec/shared/resources_spec.rb
@@ -12,17 +12,20 @@ describe Resources do
 
   describe '#get_resource' do
     it 'loads the correct YAML file' do
-      mock(File).read(File.join(TwitterCldr::RESOURCES_DIR, 'shared/currencies.yml')) { "---\n- 1\n- 2\n" }
+      stub(File).read(File.join(TwitterCldr::RESOURCES_DIR, 'shared/currencies.yml')) { "---\n- 1\n- 2\n" }
       resources.get_resource(:shared, :currencies).should == [1, 2]
     end
 
     it 'loads the resource only once' do
       mock(resources).load_resource('shared/currencies.yml').once { 'foo-bar-baz' }
-      5.times { resources.get_resource(:shared, :currencies).should == 'foo-bar-baz' }
+
+      result = resources.get_resource(:shared, :currencies)
+      # second time load_resource is not called but we get the same object as before
+      resources.get_resource(:shared, :currencies).object_id.should == result.object_id
     end
 
     it 'accepts a variable length resource path both in symbols and strings' do
-      mock(resources).load_resource('foo/bar/baz.yml') { 'foo-bar-baz' }
+      stub(resources).load_resource('foo/bar/baz.yml') { 'foo-bar-baz' }
       resources.get_resource('foo', :bar, 'baz').should == 'foo-bar-baz'
     end
 
@@ -33,12 +36,12 @@ describe Resources do
 
   describe '#get_locale_resource' do
     it 'load the correct locale resource file' do
-      mock(resources).get_resource(:locales, :de, :numbers) { 'foo' }
+      stub(resources).get_resource(:locales, :de, :numbers) { 'foo' }
       resources.get_locale_resource(:de, :numbers).should == 'foo'
     end
 
     it 'converts locales' do
-      mock(TwitterCldr).convert_locale('zh-tw') { :'zh-Hant'}
+      mock(TwitterCldr).convert_locale('zh-tw') { :'zh-Hant' }
       mock(resources).get_resource(:locales, :'zh-Hant', :numbers) { 'foo' }
 
       resources.get_locale_resource('zh-tw', :numbers).should == 'foo'

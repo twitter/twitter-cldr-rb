927ae516d106bee28af4847e1390c7c4db45e227
Cameron Dutro <cdutro@twitter.com>
2012-06-14 17:10:51 -0700

    Removing redundant spec file

--- spec/normalizers/nfd_spec.rb
+++
@@ -1,23 +0,0 @@
-# encoding: UTF-8
-
-# Copyright 2012 Twitter, Inc
-# http://www.apache.org/licenses/LICENSE-2.0
-
-require 'spec_helper'
-
-include TwitterCldr::Normalizers
-
-describe NFD do
-
-  describe "#normalize" do
-    it "should normalize certain types of characters successfully" do
-      NFD.normalize("庠摪饢鼢豦樄澸脧鱵礩翜艰").should == "庠摪饢鼢豦樄澸脧鱵礩翜艰"
-      NFD.normalize("䷙䷿").should == "䷙䷿"
-      NFD.normalize("ᎿᎲᎪᏨᎨᏪᎧᎵᏥ").should == "ᎿᎲᎪᏨᎨᏪᎧᎵᏥ"
-      NFD.normalize("ᆙᅓᆼᄋᇶ").should == "ᆙᅓᆼᄋᇶ"
-      NFD.normalize("…‾⁋ ⁒⁯‒′‾⁖").should == "…‾⁋ ⁒⁯‒′‾⁖"
-      NFD.normalize("ⶾⷕⶱⷀ").should == "ⶾⷕⶱⷀ"
-    end
-  end
-
-end

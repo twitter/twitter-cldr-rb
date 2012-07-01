65d1d6553fe5d4e8573647b1398a6891c423bf97
Kirill Lashuk <kirill.lashuk@gmail.com>
2012-07-01 19:28:55 +0300

    Convert TrieBuilder into a module.

--- lib/twitter_cldr/collation/trie_builder.rb
+++ lib/twitter_cldr/collation/trie_builder.rb
@@ -8,46 +8,42 @@ module TwitterCldr
 
     # Builds a collation elements Trie from the file containing a fractional collation elements table.
     #
-    class TrieBuilder
+    module TrieBuilder
 
       FRACTIONAL_UCA_REGEXP = /^((?:[0-9A-F]+)(?:\s[0-9A-F]+)*);\s((?:\[.*?\])(?:\[.*?\])*)/
 
-      def self.load_trie(file_path)
-        new(file_path).build
-      end
+      class << self
 
-      def initialize(resource)
-        @file_path = File.join(TwitterCldr::RESOURCES_DIR, resource)
-      end
-
-      def build
-        parse_trie(load_collation_elements_table)
-      end
-
-      private
-
-      def parse_trie(table)
-        trie = TwitterCldr::Collation::Trie.new
-
-        table.lines.each do |line|
-          trie.add(parse_code_points($1), parse_collation_element($2)) if FRACTIONAL_UCA_REGEXP =~ line
+        def load_trie(resource)
+          parse_trie(load_resource(resource))
         end
 
-        trie
-      end
+        private
 
-      def load_collation_elements_table
-        open(@file_path, 'r')
-      end
+        def parse_trie(table)
+          trie = TwitterCldr::Collation::Trie.new
 
-      def parse_code_points(string)
-        string.split.map { |cp| cp.to_i(16) }
-      end
+          table.lines.each do |line|
+            trie.add(parse_code_points($1), parse_collation_element($2)) if FRACTIONAL_UCA_REGEXP =~ line
+          end
 
-      def parse_collation_element(string)
-        string.scan(/\[.*?\]/).map do |match|
-          match[1..-2].gsub(/\s/, '').split(',', -1).map { |bytes| bytes.to_i(16) }
+          trie
         end
+
+        def load_resource(resource)
+          open(File.join(TwitterCldr::RESOURCES_DIR, resource), 'r')
+        end
+
+        def parse_code_points(string)
+          string.split.map { |cp| cp.to_i(16) }
+        end
+
+        def parse_collation_element(string)
+          string.scan(/\[.*?\]/).map do |match|
+            match[1..-2].gsub(/\s/, '').split(',', -1).map { |bytes| bytes.to_i(16) }
+          end
+        end
+
       end
 
     end

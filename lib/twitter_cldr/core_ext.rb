521e37c79addae3723be724aa9c34d5e764974c2
Cameron Dutro <cdutro@twitter.com>
2012-08-30 22:21:22 +0000

    Fixing wonky merge issues

---
+++ lib/twitter_cldr/core_ext.rb
@@ -0,0 +1,12 @@
+# encoding: UTF-8
+
+# Copyright 2012 Twitter, Inc
+# http://www.apache.org/licenses/LICENSE-2.0
+
+[Bignum, Fixnum, Float].each do |klass|
+  TwitterCldr::Localized::LocalizedNumber.localize(klass)
+end
+
+[Array, Date, DateTime, String, Symbol, Time].each do |klass|
+  TwitterCldr::Localized::const_get("Localized#{klass}").localize(klass)
+end

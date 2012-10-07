9514e82958f3cb7294013b26778536dc63cd845e
Cameron Dutro <cdutro@twitter.com>
2012-10-07 15:24:55 -0700

    Adding layout support

--- lib/twitter_cldr/resources/locales_resources_importer.rb
+++ lib/twitter_cldr/resources/locales_resources_importer.rb
@@ -12,7 +12,7 @@ module TwitterCldr
 
     class LocalesResourcesImporter
 
-      COMPONENTS = %w[calendars languages numbers units plurals lists]
+      COMPONENTS = %w[calendars languages numbers units plurals lists layout]
 
       # Arguments:
       #

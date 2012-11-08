2cc0f0b22af97e9b9f1588682daf5514a8da0c8b
Cameron Dutro <cdutro@twitter.com>
2012-11-08 13:14:36 -0800

    Fixing date formatter now that timespans have been updated

--- spec/localized/localized_date_spec.rb
+++ spec/localized/localized_date_spec.rb
@@ -25,7 +25,7 @@ describe LocalizedDate do
       loc_date = date.localize(:en)
       loc_date.ago(:base_time => base_time).to_s.should match_normalized("1 month ago")
       loc_date.ago(:base_time => Time.gm(2010,12,6,12,12,30)).to_s.should match_normalized("5 months ago")
-      loc_date.ago(:base_time => Time.gm(2010,7,7,12,12,30)).to_s.should match_normalized("1 day ago")
+      loc_date.ago(:base_time => Time.gm(2010,7,7,12,12,30)).to_s.should match_normalized("2 days ago")
       loc_date.ago(:base_time => Time.gm(2010,7,6,12,12,30)).to_s.should match_normalized("12 hours ago")
       loc_date.ago(:base_time => Time.gm(2010,7,6,0,39,0)).to_s.should match_normalized("39 minutes ago")
     end
@@ -33,24 +33,24 @@ describe LocalizedDate do
     it "should ago-ify with strings regardless of variable's placement or existence" do
       loc_date = date.localize(:ar)
       loc_date.ago(:base_time => base_time).to_s(:unit => :hour).should match_normalized("قبل 756 ساعة")
-      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("قبل 31 يومًا")
+      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("قبل 32 يومًا")
       loc_date.ago(:base_time => base_time).to_s(:unit => :month).should match_normalized("قبل شهر واحد")
       loc_date.ago(:base_time => base_time).to_s(:unit => :year).should match_normalized("قبل 0 سنة")
 
       loc_date = date.localize(:fa)
-      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("31 روز پیش")
+      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("32 روز پیش")
 
       loc_date = date.localize(:en)
-      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("31 days ago")
+      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("32 days ago")
     end
 
     it "should ago-ify a date with a number of different units" do
       date = Date.new(2010,6,6)
       loc_date = date.localize(:de)
       loc_date.ago(:base_time => base_time).to_s(:unit => :second).should match_normalized("Vor 5314350 Sekunden")
-      loc_date.ago(:base_time => base_time).to_s(:unit => :minute).should match_normalized("Vor 88572 Minuten")
+      loc_date.ago(:base_time => base_time).to_s(:unit => :minute).should match_normalized("Vor 88573 Minuten")
       loc_date.ago(:base_time => base_time).to_s(:unit => :hour).should match_normalized("Vor 1476 Stunden")
-      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("Vor 61 Tagen")
+      loc_date.ago(:base_time => base_time).to_s(:unit => :day).should match_normalized("Vor 62 Tagen")
       loc_date.ago(:base_time => base_time).to_s(:unit => :month).should match_normalized("Vor 2 Monaten")
       loc_date.ago(:base_time => base_time).to_s(:unit => :year).should match_normalized("Vor 0 Jahren")
     end
@@ -69,8 +69,8 @@ describe LocalizedDate do
       date = Date.new(2010,10,10)
       loc_date = date.localize(:de)
       loc_date.until(:base_time => base_time).to_s(:unit => :second).should match_normalized("In 5572050 Sekunden")
-      loc_date.until(:base_time => base_time).to_s(:unit => :minute).should match_normalized("In 92867 Minuten")
-      loc_date.until(:base_time => base_time).to_s(:unit => :hour).should match_normalized("In 1547 Stunden")
+      loc_date.until(:base_time => base_time).to_s(:unit => :minute).should match_normalized("In 92868 Minuten")
+      loc_date.until(:base_time => base_time).to_s(:unit => :hour).should match_normalized("In 1548 Stunden")
       loc_date.until(:base_time => base_time).to_s(:unit => :day).should match_normalized("In 64 Tagen")
       loc_date.until(:base_time => base_time).to_s(:unit => :month).should match_normalized("In 2 Monaten")
       loc_date.until(:base_time => base_time).to_s(:unit => :year).should match_normalized("In 0 Jahren")

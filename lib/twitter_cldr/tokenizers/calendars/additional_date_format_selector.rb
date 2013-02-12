dcaa5bc9d5819bfe7ef8c52591359e76b8c9cc60
Cameron Dutro <cdutro@twitter.com>
2013-02-12 15:12:54 -0800

    Fixing exact matches even more

--- lib/twitter_cldr/tokenizers/calendars/additional_date_format_selector.rb
+++ lib/twitter_cldr/tokenizers/calendars/additional_date_format_selector.rb
@@ -17,8 +17,8 @@ module TwitterCldr
           nil
         else
           cache_key = TwitterCldr::Utils.compute_cache_key(goal_pattern)
-          pattern_cache[cache_key] ||= if @pattern_hash.include?(goal_pattern)
-            @pattern_hash[goal_pattern]
+          pattern_cache[cache_key] ||= if @pattern_hash.include?(goal_pattern.to_sym)
+            @pattern_hash[goal_pattern.to_sym]
           else
             rank(goal_pattern).min do |(p1, score1), (p2, score2)|
               score1 <=> score2

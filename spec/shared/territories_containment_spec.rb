2933ec3831de4c68e11b93354b1fd4e6765756e5
Kirill Lashuk <klashuk@twitter.com>
2014-08-06 18:24:50 -0700

    Fix territories containment resources.

--- spec/shared/territories_containment_spec.rb
+++ spec/shared/territories_containment_spec.rb
@@ -14,7 +14,7 @@ describe TerritoriesContainment do
     end
 
     it 'returns nil when given a top-level territory' do
-      expect(TerritoriesContainment.parent('1')).to eq(nil)
+      expect(TerritoriesContainment.parent('001')).to eq(nil)
     end
 
     it 'raises an exception when given an invalid territory code' do
@@ -25,6 +25,10 @@ describe TerritoriesContainment do
   describe '.children' do
     it 'returns the immediate children of the territory' do
       expect(TerritoriesContainment.children('151')).to eq(%w[BG BY CZ HU MD PL RO RU SK SU UA])
+    end
+
+    it 'returns codes with leading zeros' do
+      expect(TerritoriesContainment.children('009')).to eq(%w[053 054 057 061 QO])
     end
 
     it 'returns an empty array when given a bottom-level territory' do
@@ -46,7 +50,7 @@ describe TerritoriesContainment do
     end
 
     it 'returns true if the first territory is a top-level territory' do
-      expect(TerritoriesContainment.contains('1', '145')).to be_true
+      expect(TerritoriesContainment.contains('001', '145')).to be_true
     end
 
     it 'returns false if the first territory does not contain the second one' do
@@ -54,7 +58,7 @@ describe TerritoriesContainment do
     end
 
     it 'returns false if the second territory is a top-level territory' do
-      expect(TerritoriesContainment.contains('419', '1')).to be_false
+      expect(TerritoriesContainment.contains('419', '001')).to be_false
     end
 
     it 'returns false if both territories are identical' do

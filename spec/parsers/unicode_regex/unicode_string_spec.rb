58be7cef5942aee701a0c00ede00cec1096f1575
Arnav Kumar <arnav@arnavkumar.com>
2014-08-22 00:12:03 +0800

    Adding a test for the edge case in `unicode_string.rb`
    - This test would fail if the typo from [130](https://github.com/twitter/twitter-cldr-rb/pull/130) was not corrected.

--- spec/parsers/unicode_regex/unicode_string_spec.rb
+++ spec/parsers/unicode_regex/unicode_string_spec.rb
@@ -18,5 +18,10 @@ describe UnicodeRegexParser::UnicodeString do
       str = UnicodeRegexParser::UnicodeString.new([97, 98, 99])
       expect(str.to_set.to_a).to eq([[97, 98, 99]..[97, 98, 99]])
     end
+
+    it "should covert the codepoints to a valid regex" do
+      str = UnicodeRegexParser::UnicodeString.new(97)
+      expect(str.to_regexp_str).to eq"(?:\\141)"
+    end
   end
 end

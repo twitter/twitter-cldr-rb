b0b7fd39695ef796809b7bf7846895abdac263a7
Cameron Dutro <camertron@gmail.com>
2015-09-27 22:21:33 -0700

    Alphabetizing list of autoloads in Utils module

--- lib/twitter_cldr/utils.rb
+++ lib/twitter_cldr/utils.rb
@@ -7,13 +7,12 @@ module TwitterCldr
   module Utils
 
     autoload :CodePoints,     'twitter_cldr/utils/code_points'
-    autoload :YAML,           'twitter_cldr/utils/yaml'
     autoload :RangeHelpers,   'twitter_cldr/utils/range_helpers'
     autoload :RangeSet,       'twitter_cldr/utils/range_set'
-    autoload :RangeHash,      'twitter_cldr/utils/range_hash'
     autoload :RegexpAst,      'twitter_cldr/utils/regexp_ast'
     autoload :RegexpSampler,  'twitter_cldr/utils/regexp_sampler'
     autoload :ScriptDetector, 'twitter_cldr/utils/script_detector'
+    autoload :YAML,           'twitter_cldr/utils/yaml'
 
     class << self
 

dc09abd04233af2fbc13c4ab144b060f9ba8351f
Cameron Dutro <camertron@gmail.com>
2015-10-02 17:20:02 -0500

    Pull postcodes from http://i18napis.appspot.com since they've been deprecated in CLDR v27

--- lib/twitter_cldr/resources/postal_codes_importer.rb
+++ lib/twitter_cldr/resources/postal_codes_importer.rb
@@ -3,49 +3,61 @@
 # Copyright 2012 Twitter, Inc
 # http://www.apache.org/licenses/LICENSE-2.0
 
-require 'nokogiri'
-
-require 'twitter_cldr/resources/download'
+require 'rest-client'
+require 'json'
+require 'yaml'
 
 module TwitterCldr
   module Resources
 
     class PostalCodesImporter
 
-      POSTAL_CODES_PATH = File.join('common', 'supplemental', 'postalCodeData.xml')
+      BASE_URL = 'http://i18napis.appspot.com/address/data/'
 
       # Arguments:
       #
-      #   input_path  - path to a directory containing CLDR data
       #   output_path - output directory for generated YAML file
       #
-      def initialize(input_path, output_path)
-        @input_path  = input_path
+      def initialize(output_path)
         @output_path = output_path
       end
 
       def import
-        TwitterCldr::Resources.download_cldr_if_necessary(@input_path)
-
-        doc = File.open(File.join(@input_path, POSTAL_CODES_PATH)) { |file| Nokogiri::XML(file) }
-
-        postal_codes = doc.xpath('//postCodeRegex').inject({}) do |memo, node|
-          memo[node.attr('territoryId').downcase.to_sym] = Regexp.new(node.text); memo
-        end
-
-        postal_codes = Hash[postal_codes.sort_by(&:first)]
-
-        postal_codes = postal_codes.each_with_object({}) do |(territory, regex), memo|
-          memo[territory] = {
-            regex: regex,
-            ast: TwitterCldr::Utils::RegexpAst.dump(
-              RegexpAstGenerator.generate(regex.source)
-            )
-          }
-        end
-
         File.open(File.join(@output_path, 'postal_codes.yml'), 'w') do |output|
-          output.write(YAML.dump(postal_codes))
+          output.write(YAML.dump(load))
+        end
+      end
+
+      private
+
+      def load
+        each_territory.each_with_object({}) do |territory, ret|
+          regex = get_regex_for(territory)
+
+          if regex
+            ret[territory] = {
+              regex: Regexp.compile(regex),
+              ast: TwitterCldr::Utils::RegexpAst.dump(
+                RegexpAstGenerator.generate(regex)
+              )
+            }
+          end
+        end
+      end
+
+      def get_regex_for(territory)
+        result = RestClient.get("#{BASE_URL}#{territory.to_s.upcase}")
+        data = JSON.parse(result.body)
+        data['zip']
+      end
+
+      def each_territory
+        if block_given?
+          TwitterCldr::Shared::Territories.all.each_pair do |territory, _|
+            yield territory
+          end
+        else
+          to_enum(__method__)
         end
       end
 
